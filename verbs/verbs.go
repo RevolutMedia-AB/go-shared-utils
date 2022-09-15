b8789fbb1f4030183427ca9324785b50a6b01bd8
rautellin <nathalie@rautellinn.com>
2022-09-15 10:42:29 +0200

    added verbs.go

---
+++ verbs/verbs.go
@@ -0,0 +1,10 @@
+package verbs
+
+const (
+	ChallengeCreated    string = "challenge_created"
+	EntryCreated        string = "entry_created"
+	FollowedChallenge   string = "followed_challenge"
+	FollowedUser        string = "followed_user"
+	UnfollowedChallenge string = "unfollowed_challenge"
+	UnfollowedUser      string = "unfollowed_user"
+)
