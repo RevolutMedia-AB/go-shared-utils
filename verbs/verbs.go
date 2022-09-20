f6bcc46274cb5c23d260fd114cc94e04911dd10f
rautellin <nathalie@rautellinn.com>
2022-09-20 12:43:16 +0200

    updated the verbs.go

--- verbs/verbs.go
+++ verbs/verbs.go
@@ -1,8 +1,8 @@
 package verbs
 
 const (
-	ChallengeCreated    string = "challenge_created"
-	EntryCreated        string = "entry_created"
+	CreatedChallenge    string = "created_challenge"
+	CreatedEntry        string = "created_entry"
 	FollowedChallenge   string = "followed_challenge"
 	FollowedUser        string = "followed_user"
 	UnfollowedChallenge string = "unfollowed_challenge"
